32b4594fc9e7ff03763393701b4e817b6ef64341
nison79 <georgenison@gmail.com>
2022-01-08 17:55:05 +0200

    make the api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'json-server-api';
+  public users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    console.log('ONINIT');
+    this.http.get('http://localhost:3000/users').subscribe((users) => {
+      console.log(users);
+
+      this.users = users;
+    });
+  }
 }
